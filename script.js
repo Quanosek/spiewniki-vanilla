b6894dcad5b734d4fb302dad3cbf2909ea58f824
Krist0f0l0s <kb.olszewski@outlook.com>
2022-08-10 00:25:19 +0200

    big cleanup

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+//główna funkcja
+async function init() {
+  map = await getJSON();
+  addEventListeners();
+}
+
+//fetch spisu pieśni json
 async function getJSON() {
   brzask = await fetch(`./generate json/brzask.json`).then((response) => {
     return response.json();
@@ -8,69 +15,24 @@ async function getJSON() {
   nowe = await fetch(`./generate json/nowe.json`).then((response) => {
     return response.json();
   });
-
-  addEventListeners();
+  let map = new Map();
+  map.set("brzask", brzask);
+  map.set("cegielki", cegielki);
+  map.set("nowe", nowe);
+  return map;
 }
 
+//dodawnie wszytskich eventListenerów
 function addEventListeners() {
-  document
-    .querySelector("#hymnBook")
-    .addEventListener("change", changeHymnBook);
-
+  let hymnBook = document.querySelector("#hymnBook");
   let searchBox = document.querySelector("#searchBox");
-  searchBox.addEventListener("keyup", (e) => {
-    let searchResults = document.querySelector("#searchResults");
-    searchResults.innerHTML = "";
-    searchResults.style.display = "block";
-    if (hymnBook.value === "brzask") list = brzask;
-    else if (hymnBook.value === "cegielki") list = cegielki;
-    else if (hymnBook.value === "nowe") list = nowe;
-    list.forEach((hymn, index) => {
-      if (
-        textFormat(hymn.title)
-          .toLowerCase()
-          .search(textFormat(e.target.value).toLowerCase()) != -1
-      ) {
-        div = document.createElement("div");
-        div.setAttribute("id", index);
-        div.style.margin = "1rem";
-        div.innerHTML = `${hymn.title}<hr>`;
-        searchResults.appendChild(div);
+  let searchResults = document.querySelector("#searchResults");
 
-        div.addEventListener("click", (e) => {
-          searchBox.value = "";
-          let index = e.target.getAttribute("id");
-
-          searchResults.innerHTML = "";
-          searchResults.style.display = "none";
-
-          const oReq = new XMLHttpRequest();
-          oReq.addEventListener("load", reqListener);
-          oReq.open("GET", list[index].link);
-          oReq.send();
-
-          document.querySelector("#title").innerHTML = "";
-          document.querySelector("#lyrics").innerHTML = "";
-          document.querySelector(".loader").style.display = "block";
-          document.querySelector("#guide").style.display = "none";
-          e.preventDefault();
-        });
-      }
-    });
-
-    try {
-      searchResults.lastChild.lastChild.remove();
-    } catch {}
-
-    if (e.target.value == "")
-      try {
-        searchResults.innerHTML = "".style.display = "none";
-      } catch {}
-
-    e.preventDefault();
-  });
+  hymnBook.addEventListener("change", changeHymnBook);
+  searchBox.addEventListener("keyup", search);
 }
 
+//na zmiane śpiewnika chowa wyniki wyszukiwania, tekst i pokazuje wskazówki
 function changeHymnBook(e) {
   searchBox.value = "";
   searchResults.innerHTML = "";
@@ -81,10 +43,66 @@ function changeHymnBook(e) {
   e.preventDefault();
 }
 
-// podmienienie polskich znaków diakrytycznych
+//szukanie
+function search(e) {
+  searchResults.innerHTML = "";
+  searchResults.style.display = "block";
+  list = map.get(hymnBook.value);
+  list.forEach((hymn, index) => {
+    if (textFormat(hymn.title).search(textFormat(e.target.value)) != -1) {
+      let div = document.createElement("div");
+      div.setAttribute("id", index);
+      div.style.margin = "1rem";
+      div.innerHTML = `${hymn.title}<hr>`;
+      searchResults.appendChild(div);
 
-function textFormat(napis) {
-  return napis
+      //dodawanie eventlistenera do wyświetlania pieśni
+      div.addEventListener("click", selectHymn);
+    }
+  });
+
+  if (searchResults.hasChildNodes()) {
+    searchResults.lastChild.lastChild.remove();
+  }
+
+  if (e.target.value == "") {
+    searchResults.innerHTML = "";
+    searchResults.style.display = "none";
+  }
+
+  e.preventDefault();
+}
+
+//wyświetlanie pieśńi
+async function selectHymn(e) {
+  const parser = new DOMParser();
+  let title = document.querySelector("#title");
+  let lyrics = document.querySelector("#lyrics");
+  let index = e.target.getAttribute("id");
+
+  searchBox.value = "";
+  searchResults.innerHTML = "";
+  searchResults.style.display = "none";
+  title.innerHTML = "";
+  lyrics.innerHTML = "";
+  document.querySelector(".loader").style.display = "block";
+  document.querySelector("#guide").style.display = "none";
+
+  let xml = await fetch(list[index].link)
+    .then((res) => res.text())
+    .then((xml) => parser.parseFromString(xml, "text/xml"))
+    .catch((err) => console.error(err));
+
+  document.querySelector(".loader").style.display = "none";
+  title.innerHTML = xml.querySelector("title").innerHTML;
+  lyrics.innerHTML = lyricsFormat(xml.querySelector("lyrics").innerHTML);
+
+  e.preventDefault();
+}
+
+// podmienienie polskich znaków diakrytycznych
+function textFormat(text) {
+  return text
     .replace("ę", "e")
     .replace("ó", "o")
     .replace("ą", "a")
@@ -93,28 +111,16 @@ function textFormat(napis) {
     .replace("ż", "z")
     .replace("ź", "z")
     .replace("ć", "c")
-    .replace("ń", "n");
+    .replace("ń", "n")
+    .toLowerCase();
 }
 
-// wyświetlanie pieśni w .lyrics
-
-function reqListener() {
-  const parser = new DOMParser();
-  let xml = parser.parseFromString(this.responseText, "text/xml");
-  let title = document.querySelector("#title");
-  let lyrics = document.querySelector("#lyrics");
-
-  title.innerHTML = xml.querySelector("title").innerHTML;
-
-  const tekst = xml
-    .querySelector("lyrics")
-    .innerHTML.replace(/\s*(\[V\d*\]|\[C\d*\])\s*/, "")
+//oczyszczenie tekstu z tagów z xml
+function lyricsFormat(lyrics) {
+  return lyrics
+    .replace(/\s*(\[V\d*\]|\[C\d*\])\s*/, "")
     .replace(/\s*(\[V\d*\]|\[C\d*\])\s*/g, "<br/><br/>")
     .replace(/\n/g, "<br/>");
-
-  document.querySelector(".loader").style.display = "none";
-  document.querySelector("#guide").style.display = "none";
-  lyrics.innerHTML = tekst;
 }
 
-getJSON();
+init();
